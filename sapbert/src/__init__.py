43c593683b295d49f93c53c8a0fe90af65e9a212
Louise Naudin <naudin.louise@gmail.com>
2022-03-18 16:58:59 +0100

    :sparkles: Create importable project

---
+++ sapbert/src/__init__.py
@@ -0,0 +1,11 @@
+from .data_loader import (
+    DictionaryDataset,
+    MetricLearningDataset,
+    MetricLearningDataset_pairwise,
+    QueryDataset,
+    QueryDataset_COMETA,
+    QueryDataset_custom,
+    QueryDataset_pretraining,
+)
+from .metric_learning import Sap_Metric_Learning
+from .model_wrapper import Model_Wrapper
